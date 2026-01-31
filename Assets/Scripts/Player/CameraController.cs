08146a66b26a9094e2aab6deacfa309695c9139f
RedPanda <jdkyu44@naver.com>
2026-01-31 18:29:17 +0900

    feat: tilemap & camerabounds
    땅 지형을 Tilemap으로 변경, CameraBounds 구현

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    public Transform player;
+    public BoxCollider2D cameraBounds;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private float halfWidth = 8.888f;
+    private float halfHeight = 5f;
 
     private void Start()
     {
-        target = GameObject.FindWithTag("Player").transform;
+        player = GameObject.FindWithTag("Player").transform;
+        cameraBounds = GameObject.FindWithTag("CameraBounds").GetComponent<BoxCollider2D>();
+
+        minX = cameraBounds.bounds.min.x;
+        maxX = cameraBounds.bounds.max.x;
+        minY = cameraBounds.bounds.min.y;
+        maxY = cameraBounds.bounds.max.y;
     }
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        float clampedX = Mathf.Clamp(player.position.x, minX + halfWidth, maxX + halfWidth);
+        float clampedY = Mathf.Clamp(player.position.y, minY + halfHeight, maxY + halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
+
